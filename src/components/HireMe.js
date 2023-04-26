a8784153ce657c8a355d4f350bc85c611103ee5f
Ancoor Banerjee <ancoorbanerjee7@gmail.com>
2023-04-26 19:16:08 +0530

    hire me z index free

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
-    sm:right-0">
+    sm:right-0 md:z-10">
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
         <CircularText className={"fill-dark dark:fill-light animate-spin-slow duration-200"} />
         <Link
