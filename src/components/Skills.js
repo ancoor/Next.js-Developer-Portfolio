be1ac0678a74bdfa4e4f63ab95babe6875c6208a
Ancoor Banerjee <ancoorbanerjee7@gmail.com>
2023-04-26 19:30:28 +0530

    skill tag rearrangment

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,18 +34,18 @@ const Skills = () => {
         >
           Web
         </motion.div>
-        <Skill name="HTML" x="-20vw" y="-2vw" />
+        <Skill name="HTML" x="-20vw" y="2vw" />
         <Skill name="CSS" x="-5vw" y="-10vw" />
-        <Skill name="JavaScript" x="25vw" y="5vw" />
-        <Skill name="ReactJs" x="0vw" y="11vw" />
-        <Skill name="NextJs" x="-27vw" y="-10vw" />
+        <Skill name="JavaScript" x="20vw" y="6vw" />
+        <Skill name="ReactJs" x="0vw" y="12vw" />
+        <Skill name="NextJs" x="-20vw" y="-15vw" />
         <Skill name="NodeJs" x="15vw" y="-12vw" />
-        <Skill name="SQL" x="-5vw" y="-10vw" />
+        <Skill name="SQL" x="-35vw" y="-15vw" />
         <Skill name="MongoDB" x="32vw" y="-5vw" />
-        <Skill name="Nightwatch" x="0vw" y="-18vw" />
-        <Skill name="Mocha & Chai" x="-25vw" y="-18vw" />
-        <Skill name="TailwindCSS" x="-25vw" y="12vw" />
-        <Skill name="PHP" x="20vw" y="14vw" />
+        <Skill name="Nightwatch" x="0vw" y="-20vw" />
+        <Skill name="Mocha & Chai" x="-25vw" y="18vw" />
+        <Skill name="TailwindCSS" x="28vw" y="18vw" />
+        <Skill name="PHP" x="28vw" y="-18vw" />
       </div>
     </>
   );
